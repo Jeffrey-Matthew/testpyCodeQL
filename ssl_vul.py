2e9f001ac557d6c060eed49bcd6984c62ab9028f
Jeffrey Matthew <32321280+Jeffrey-Matthew@users.noreply.github.com>
2022-09-06 13:04:23 -0400

    Update ssl_vul.py

--- ssl_vul.py
+++ ssl_vul.py
@@ -27,64 +27,65 @@ def before_request():
     g.session = libsession.load(request)
 
 app.run(debug=True, host='127.0.1.1', ssl_context=('/tmp/acme.cert', '/tmp/acme.key'))
-
-import sqlite3
-import libuser
-
-
-def login(username, password):
-
-    conn = sqlite3.connect('db_users.sqlite')
-    conn.set_trace_callback(print)
-    conn.row_factory = sqlite3.Row
-    c = conn.cursor()
-
-    user = c.execute("SELECT * FROM users WHERE username = '{}' and password = '{}'".format(username, password)).fetchone()
-
-    if user:
-        return user['username']
-    else:
-        return False
+def libuser_test():
+    import sqlite3
+    import libuser
 
 
-def create(username, password):
+    def login(username, password):
 
-    conn = sqlite3.connect('db_users.sqlite')
-    c = conn.cursor()
+        conn = sqlite3.connect('db_users.sqlite')
+        conn.set_trace_callback(print)
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
 
-    c.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES ('%s', '%s', '%d', '%d', '%s')" %(username, password, 0, 0, ''))
+        user = c.execute("SELECT * FROM users WHERE username = '{}' and password = '{}'".format(username, password)).fetchone()
 
-    conn.commit()
-    conn.close()
-
-
-def userlist():
-
-    conn = sqlite3.connect('db_users.sqlite')
-    conn.set_trace_callback(print)
-    conn.row_factory = sqlite3.Row
-    c = conn.cursor()
-
-    users = c.execute("SELECT * FROM users").fetchall()
-
-    if not users:
-        return []
-    else:
-        return [ user['username'] for user in users ]
+        if user:
+            return user['username']
+        else:
+            return False
 
 
-def password_change(username, password):
+    def create(username, password):
 
-    conn = sqlite3.connect('db_users.sqlite')
-    conn.set_trace_callback(print)
-    conn.row_factory = sqlite3.Row
-    c = conn.cursor()
+        conn = sqlite3.connect('db_users.sqlite')
+        c = conn.cursor()
 
-    c.execute("UPDATE users SET password = '{}' WHERE username = '{}'".format(password, username))
-    conn.commit()
+        c.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES ('%s', '%s', '%d', '%d', '%s')" %(username, password, 0, 0, ''))
 
-    return True
+        conn.commit()
+        conn.close()
 
 
-def password_complexity(password):
-    return True
+    def userlist():
+
+        conn = sqlite3.connect('db_users.sqlite')
+        conn.set_trace_callback(print)
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+
+        users = c.execute("SELECT * FROM users").fetchall()
+
+        if not users:
+            return []
+        else:
+            return [ user['username'] for user in users ]
+
+
+    def password_change(username, password):
+
+        conn = sqlite3.connect('db_users.sqlite')
+        conn.set_trace_callback(print)
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+
+        c.execute("UPDATE users SET password = '{}' WHERE username = '{}'".format(password, username))
+        conn.commit()
+
+        return True
+
+
+    def password_complexity(password):
+        return True
+libuser_test()
