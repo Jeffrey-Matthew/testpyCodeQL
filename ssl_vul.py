a47eb611c878da6351ac2c4345f1bc808d7f357e
Jeffrey Matthew <32321280+Jeffrey-Matthew@users.noreply.github.com>
2022-09-06 13:08:45 -0400

    Update ssl_vul.py

--- ssl_vul.py
+++ ssl_vul.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python3
+def flask_test():
+    from flask import Flask, g, redirect, request
 
-from flask import Flask, g, redirect, request
+    from mod_hello import mod_hello
+    from mod_user import mod_user
+    from mod_posts import mod_posts
+    from mod_mfa import mod_mfa
 
-from mod_hello import mod_hello
-from mod_user import mod_user
-from mod_posts import mod_posts
-from mod_mfa import mod_mfa
+    import libsession
 
-import libsession
+    app = Flask('vulpy')
+    app.config['SECRET_KEY'] = 'aaaaaaa'
 
-app = Flask('vulpy')
-app.config['SECRET_KEY'] = 'aaaaaaa'
-
-app.register_blueprint(mod_hello, url_prefix='/hello')
-app.register_blueprint(mod_user, url_prefix='/user')
-app.register_blueprint(mod_posts, url_prefix='/posts')
-app.register_blueprint(mod_mfa, url_prefix='/mfa')
+    app.register_blueprint(mod_hello, url_prefix='/hello')
+    app.register_blueprint(mod_user, url_prefix='/user')
+    app.register_blueprint(mod_posts, url_prefix='/posts')
+    app.register_blueprint(mod_mfa, url_prefix='/mfa')
 
 
-@app.route('/')
-def do_home():
-    return redirect('/posts')
+    @app.route('/')
+    def do_home():
+        return redirect('/posts')
 
-@app.before_request
-def before_request():
-    g.session = libsession.load(request)
+    @app.before_request
+    def before_request():
+        g.session = libsession.load(request)
 
-app.run(debug=True, host='127.0.1.1', ssl_context=('/tmp/acme.cert', '/tmp/acme.key'))
+    app.run(debug=True, host='127.0.1.1', ssl_context=('/tmp/acme.cert', '/tmp/acme.key'))
+
+libuser_test()
+flask_test()
 def libuser_test():
     import sqlite3
     import libuser
@@ -88,4 +91,4 @@ def libuser_test():
 
     def password_complexity(password):
         return True
-libuser_test()
+
